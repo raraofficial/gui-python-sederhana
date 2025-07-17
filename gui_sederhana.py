b735ca1eb7b904244e4e1c80b7188598bf5030f4
Citra Moho <citramoho01@gmail.com>
2025-07-17 16:16:15 +0700

    Add files via upload

---
+++ gui_sederhana.py
@@ -0,0 +1,12 @@
+# GUI -> GRAPHICAL USER INTERFACE
+
+import tkinter as tk
+
+window = tk.Tk()
+window.configure(bg="white")
+window.geometry("300x200")
+window.resizable(False,False)
+window.title("Official Rara")
+
+
+window.mainloop()
